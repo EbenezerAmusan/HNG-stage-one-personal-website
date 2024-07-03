02dc42820fd167b21e74bdb91f6c70ec3527bee6
Ebenezer Amusan <114451891+EbenezerAmusan@users.noreply.github.com>
2024-07-03 23:12:10 +0100

    HNG 11 frontend stage one first commit

---
+++ Goals/index.js
@@ -0,0 +1,31 @@
+//set current time in UTC
+document.addEventListener("DOMContentLoaded", () => {
+  const currentTimeElement = document.getElementById("currentTime");
+  const updateTime = () => {
+    const now = new Date();
+    currentTimeElement.textContent = now.toUTCString();
+  };
+  updateTime();
+  setInterval(updateTime, 1000);
+
+  //set current day of the week
+  const currentDayElement = document.getElementById("currentDay");
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  currentDayElement.textContent = days[new Date().getUTCDay()];
+
+//   const profilePicture = document.getElementById('profilePicture');
+//   const setProfilePictureDimensions = () => {
+//     profilePicture.width = profilePicture.naturalWidth;
+//     profilePicture.heigth = profilePicture.naturalHeight;
+//   };
+
+  profilePicture.onload = setProfilePictureDimensions;
+});
